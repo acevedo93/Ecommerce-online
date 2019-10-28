4dc40f43dcee34eb89e5b157b4d0baf01fe3bd34
David Acevedo Jaramillo <dacevedo@administradors-DVGF1J.local>
2019-10-28 14:08:18 -0500

    add page shop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { HomeComponent} from './pages/homepage/HomePage.component'
 import { Switch, Route, Link } from 'react-router-dom'
-
+import ShopPageComponent from './pages/shoppage/ShopPage.component';
 
 const HatsPage = () => (
   <div>
@@ -15,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomeComponent}  />
         <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPageComponent} />
       </Switch>
     </div>
   );
